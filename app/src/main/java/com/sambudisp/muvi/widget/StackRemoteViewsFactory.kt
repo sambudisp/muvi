1238b7f15a621e979c7f73eb77e5a5a351c1c91f
Sambudi Sugya Putra <sambudisp@gmail.com>
2020-03-11 09:49:15 +0700

    .

--- app/src/main/java/com/sambudisp/muvi/widget/StackRemoteViewsFactory.kt
+++ app/src/main/java/com/sambudisp/muvi/widget/StackRemoteViewsFactory.kt
@@ -2,10 +2,13 @@ package com.sambudisp.muvi.widget
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.os.AsyncTask
 import android.util.Log
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
 import androidx.core.os.bundleOf
+import com.bumptech.glide.Glide
 import com.sambudisp.muvi.FavWidget
 import com.sambudisp.muvi.R
 import com.sambudisp.muvi.adapter.FavAdapter
@@ -21,23 +24,25 @@ class StackRemoteViewsFactory(private val context: Context) :
     RemoteViewsService.RemoteViewsFactory {
 
     private var content = ArrayList<FavModel>()
+    private var contentX = ArrayList<FavModel>()
     private lateinit var adapter: FavAdapter
     private lateinit var favHelper: FavHelper
 
-    private val widgetItem = ArrayList<String>()
-    private var isi : String? = null
+    private val widgetItem = ArrayList<Bitmap>()
+    //private val widgetItem = ArrayList<String>()
+    private var isi: String? = null
 
     override fun onCreate() {
         favHelper = FavHelper.getInstance(context)
         if (!favHelper.isOpen()) favHelper.open()
     }
 
-    private fun getFavData() {
+    override fun onDataSetChanged() {
         try {
             val cursor = favHelper.queryAll()
             val resultCursor = MappingHelper.mapCursorToArrayList(cursor)
             if (resultCursor.size > 0) {
-                GlobalScope.launch(Dispatchers.Main) {
+                GlobalScope.launch(Dispatchers.IO) {
                     val deferredFavs = async(Dispatchers.IO) {
                         val cursorSearch = favHelper.queryAll()
                         MappingHelper.mapCursorToArrayList(cursorSearch)
@@ -46,10 +51,14 @@ class StackRemoteViewsFactory(private val context: Context) :
                     if (fav.size > 0) {
                         content.clear()
                         content = fav
-                        for (i in 0 until content.size){
-                            isi = content[i].title.toString()
-                            onDataSetChanged()
-                            widgetItem.add(isi.toString())
+                        for (i in 0 until content.size) {
+                            val poster = content[i].poster.toString()
+                            val bitmap = Glide.with(context)
+                                .asBitmap()
+                                .load("https://image.tmdb.org/t/p/w342" + poster)
+                                .submit()
+                                .get()
+                            widgetItem.add(bitmap)
                         }
                     } else {
                         content.clear()
@@ -62,21 +71,31 @@ class StackRemoteViewsFactory(private val context: Context) :
         }
     }
 
-    override fun onDataSetChanged() {
-        widgetItem.add("Data")
-    }
-
     override fun getViewAt(position: Int): RemoteViews {
         val rv = RemoteViews(context.packageName, R.layout.item_widget_fav)
-        rv.setTextViewText(R.id.txt_banner_widget_fav, widgetItem[position])
+        rv.setImageViewBitmap(R.id.img_banner_widget_fav, widgetItem[position])
 
         val extras = bundleOf(
             FavWidget.EXTRA_ITEM to position
         )
+
         val fillInIntent = Intent()
         fillInIntent.putExtras(extras)
-        rv.setOnClickFillInIntent(R.id.txt_banner_widget_fav, fillInIntent)
+        rv.setOnClickFillInIntent(R.id.img_banner_widget_fav, fillInIntent)
         return rv
+
+//         val rv = RemoteViews(context.packageName, R.layout.item_widget_fav)
+//        rv.setTextViewText(R.id.txt_banner_widget_fav, widgetItem[position])
+//
+//        val extras = bundleOf(
+//            FavWidget.EXTRA_ITEM to position
+//        )
+//
+//        val fillInIntent = Intent()
+//        fillInIntent.putExtras(extras)
+//        rv.setOnClickFillInIntent(R.id.txt_banner_widget_fav, fillInIntent)
+//        return rv
+//
     }
 
     override fun getCount(): Int = widgetItem.size
